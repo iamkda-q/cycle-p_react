fdabfb4711fbfc10825d27b0bb76bd163a869580
KDA <iamkda@yandex.ru>
2023-03-03 22:21:19 +0300

    fix: rewrite Gallery scss

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -18,32 +18,6 @@ function Gallery() {
         setValue(e.target.id);
     }
 
-    // const els = document.querySelectorAll("[type='radio']");
-    // for (const el of els)
-    //     el.addEventListener("input", (e) => reorder(e.target, els));
-    // reorder(els[0], els);
-
-    // function reorder(targetEl, els) {
-    //     const nItems = els.length;
-    //     let processedUncheck = 0;
-    //     for (const el of els) {
-    //         const containerEl = el.nextElementSibling;
-    //         if (el === targetEl) {
-    //             //checked radio
-    //             containerEl.style.setProperty("--w", "100%");
-    //             containerEl.style.setProperty("--l", "0");
-    //         } else {
-    //             //unchecked radios
-    //             containerEl.style.setProperty("--w", `${100 / (nItems - 1)}%`);
-    //             containerEl.style.setProperty(
-    //                 "--l",
-    //                 `${(processedUncheck * 100) / (nItems - 1)}%`
-    //             );
-    //             processedUncheck += 1;
-    //         }
-    //     }
-    // }
-
     return (
         <div className="gallery">
             <div className="gallery__wrapper" ref={galleryRef}>
